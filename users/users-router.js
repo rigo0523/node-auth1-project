a564f94090dee087fbf87425b385c5c275b11562
Rigo <limon1285@gmail.com>
2021-07-15 22:37:09 -0500

    patch, delete endpoints added

--- users/users-router.js
+++ users/users-router.js
@@ -20,4 +20,25 @@ router.get("/:id", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+//PATCH /api/users/:id --> only able to update user email
+router.patch("/:id", (req, res, next) => {
+  const { id } = req.params;
+  Users.updateUserEmail(id, { email: req.body.email })
+    .then((user) => {
+      console.log(user);
+      res.json(user);
+    })
+    .catch((err) => next(err));
+});
+
+//DELETE api/users/:id --->
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  Users.deleteUser(id)
+    .then((user) => {
+      res.json({ user_deleted: `deleted Id: ${id}, ${req.body.username}` });
+    })
+    .catch((err) => next(err));
+});
+
 module.exports = router;
